73879424a4b1ee61a1132256b4080976c86f36e2
ymd65536 <ymd65536@gmail.com>
2021-10-08 16:07:08 +0900

    Main

---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Text;
+using System.Text.Json;
+using System.IO;
+using System.IO.Compression;
+
+namespace WebDriverDownload
+{
+    class Program
+    {
+        static void Main()
+        {
+            // アプリケーションの設定を読み込み
+            string CurDir = Directory.GetCurrentDirectory();
+            StreamReader AppSetting = new StreamReader(CurDir + "\\" + "appConfig.json");
+            JsonDocument appConfig = JsonDocument.Parse(AppSetting.ReadToEnd());
+            JsonElement appElement = appConfig.RootElement.GetProperty("app");
+            //JsonElement GCConfig = appConfig.RootElement.GetProperty("GCConfig");
+
+            string jsonPath = appElement[0].GetProperty("AuthJsonFile").ToString();
+            string encode = appElement[0].GetProperty("AuthJsonFileEncode").ToString();
+            string extractPath = appElement[0].GetProperty("ExtractFolderPath").ToString();
+
+            // ブラウザのインストール先を変数にロード
+            JsonElement MSEdgeConfig = appConfig.RootElement.GetProperty("MSEdgeConfig");
+            string MSEdgeZipFilePath = MSEdgeConfig.GetProperty("ZipFilePath").ToString();
+            string MSEdgeZipFileName = MSEdgeConfig.GetProperty("ZipFileName").ToString();
+            string MSEdgePath = MSEdgeConfig.GetProperty("Path").ToString();
+            string MSEdgeFileName = MSEdgeConfig.GetProperty("FileName").ToString();
+            string MSEdgeWebDriverPath = MSEdgeConfig.GetProperty("WebDriverPath").ToString();
+            string MSEdgeWebDriverFileName = MSEdgeConfig.GetProperty("WebDriverFileName").ToString();
+            string MSEdgeRequestUrl = MSEdgeConfig.GetProperty("RequestEndPoint").ToString();
+            string MSEdgeVersion = AppVersionCheck.AppVersionCheck.GetVersion(MSEdgePath, MSEdgeFileName);
+            string MSEdgeWebDriverVersion = AppVersionCheck.AppVersionCheck.GetVersion(MSEdgeWebDriverPath, MSEdgeWebDriverFileName);
+
+            string ZipFileRequestUrl = MSEdgeRequestUrl + MSEdgeVersion + "/" + MSEdgeZipFileName;
+
+            // バージョンチェック
+            // ダウンロードするかどうかの判定
+            if (MSEdgeVersion == MSEdgeWebDriverVersion)
+            {
+                Console.WriteLine("WebDriver アップデート済");
+                return;
+            }
+            else
+            {
+                try
+                {
+
+                    // プロキシ接続
+                    // WebDriverをダウンロード
+                    // Zipファイルを出力
+                    ProxyInfo.ProxyConfig SetConfig = new ProxyInfo.ProxyConfig();
+                    UserInfo ConfigData = new UserInfo();
+                    ConfigData = SetConfig.ReadJson(jsonPath, Encoding.GetEncoding(encode));
+
+                    if (!(SetConfig == null))
+                    {
+                        Console.WriteLine("ファイルリクエスト：" + ZipFileRequestUrl);
+                        ConnectProxy.ProxyConnection ProCon = new ConnectProxy.ProxyConnection();
+                        ProCon.Connect(ConfigData);
+                        ProCon.RequestFile(ZipFileRequestUrl, MSEdgeZipFilePath);
+                    }
+
+                }
+                catch
+                {
+                    return;
+                }
+
+                // Zipファイルを解凍
+                Console.WriteLine("zipファイルの解凍開始！！：" + MSEdgeZipFilePath);
+                Console.WriteLine(MSEdgeZipFilePath + "=>" + extractPath);
+
+                try
+                {
+                    if (Directory.Exists(extractPath))
+                    {
+                        Directory.Delete(extractPath, true);
+                    }
+                    ZipFile.ExtractToDirectory(MSEdgeZipFilePath, extractPath);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("解凍に失敗しました。");
+                    Console.WriteLine(e.Message);
+                }
+
+            }
+            Console.WriteLine("インストール終了！！");
+            return;
+        }
+    }
+}
